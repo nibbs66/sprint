7fa3e14a2d7a656da3b382e3478af0b66a0e79d7
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2023-02-03 15:54:00 +0100

    first change

--- web/src/components/Gallery/Gallery.js
+++ web/src/components/Gallery/Gallery.js
@@ -28,7 +28,7 @@ export default function Example() {
   return (
     <ul
       role="list"
-      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
     >
       {[...Array(4)].map((file, idx) => (
         <li key={idx} className="relative">
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,19 +3,26 @@ import { MetaTags } from '@redwoodjs/web'
 
 import Contact from 'src/components/Contact/Contact'
 import Gallery from 'src/components/Gallery/Gallery'
+import HomeMap from 'src/components/HomeMap/HomeMap'
 import Intro from 'src/components/Intro/Intro'
 import Nieuws from 'src/components/Nieuws/Nieuws'
+import SharedContent from 'src/components/SharedContent/SharedContent'
 import Sponsors from 'src/components/Sponsors/Sponsors'
+import Upcoming from 'src/components/Upcoming/Upcoming'
 const HomePage = () => {
   return (
-    <>
+    <div className={`flex flex-col space-y-5 items-center `}>
       <MetaTags title="Home" description="Home page" />
       <Intro />
-      <Nieuws />
-      <Gallery/>
-      <Sponsors/>
-      <Contact/>
-    </>
+      <div className={`w-full`}>
+        <HomeMap />
+      </div>
+      <SharedContent />
+
+      <Gallery />
+      <Sponsors />
+      <Contact />
+    </div>
   )
 }
 
